5b61ca656e689893ee930b862fb0ad90b7aa0ca2
littmir <littmir@gmail.com>
2025-04-07 00:08:46 +0700

    change cut to awk

--- sway/sway-bar.sh
+++ sway/sway-bar.sh
@@ -7,14 +7,14 @@ battery_status=$(cat /sys/class/power_supply/BAT1/status)
 bat_percentage=$(upower -i /org/freedesktop/UPower/devices/battery_BAT1 | grep  "percentage" | sed 's/percentage:          //' | sed 's/   //')
 
 
-disk_used=$(df -h | grep /home | cut -f10 -d' ')
-disk_free=$( df -h | grep /home | cut -f12 -d' ')
-disk_percentage=$( df -h | grep /home | cut -f15 -d' ')
+disk_used=$(df -h | grep /home | awk '{print $3}')
+disk_total=$(df -h | grep /home | awk '{print $2}')
+disk_percentage=$(df -h | grep /home | awk '{print $3/$2 * 100.0}' | cut -d. -f1)
 
 ram_total=$(free -h | grep Mem | cut -f13 -d' ')
 ram_free=$(free -h | grep Mem | cut -f20 -d' ')
-ram_percentage=$(free -h | grep Mem | awk '{print $3/$2 * 100.0}')
+ram_percentage=$(free -h | grep Mem | awk '{print $3/$2 * 100.0}' | cut -d. -f1)
 
 cpu_usage=$(top -bn1 | grep '%Cpu' | tail -1 | grep -P '(....|...) id,'| awk '{print "CPU Usage: " 100-$8 "%"}')
 
-echo  "RAM Usage: $ram_free/$ram_total ($ram_percentage%) | $cpu_usage | Disk Usage: $disk_used/$disk_free ($disk_percentage) | Bat $battery_status$bat_percentage | $current_time"
+echo  "RAM Usage: $ram_free/$ram_total ($ram_percentage%) | $cpu_usage | Disk Usage: $disk_used/$disk_total ($disk_percentage%) | Bat $battery_status$bat_percentage | $current_time"
